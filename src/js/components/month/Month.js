cfd642271b515e59064ce51f68c7d4b5a64f8525
Gustavo Bueno <gustavo.bueno@keradgames.com>
2018-05-10 17:19:50 +0200

    month days in progress

--- src/js/components/month/Month.js
+++ src/js/components/month/Month.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import moment from 'moment'
 
 import styles from './Month.scss'
+
+const renderWeekDays = () => {
+  return moment.weekdays().map((weekDay, index) => {
+    return (
+      <div key={index} className={styles['week-day']}>
+        <span>{weekDay}</span>
+      </div>
+    )
+  })
+}
+
+const renderMonthDays = (daysInMonth) => {
+  return Array(daysInMonth).fill().map((_, i) => {
+    const dayNumber = i + 1
+    return <span key={i}>{dayNumber}</span>
+  })
+}
 
 const Month = ({ month, daysInMonth }) => {
   return (
     <div className={styles.month}>
-      month
+      <div className={styles['month-header']}>
+        {renderWeekDays()}
+      </div>
+      <div className={styles['month-days']}>
+        {renderMonthDays(daysInMonth)}
+      </div>
     </div>
   )
 }
